4de323cf6d97319df9768180cbd6a0bf8f5e8ae8
Sy <767854885@qq.com>
2024-11-04 15:24:08 +0800

    #15、读取屏幕分辨率大小

--- Assets/Project/10、网格合并/MeshCombineTest.cs
+++ Assets/Project/10、网格合并/MeshCombineTest.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 
 public class MeshCombineTest : MonoBehaviour
 {
-    [MenuItem("GameObject/合并网格",priority = 0)]
+    //[MenuItem("GameObject/合并网格",priority = 0)]
     public static void MeshCombine()
     {
-        MeshFilter[] meshFilters = Selection.activeGameObject.GetComponentsInChildren<MeshFilter>();
-        CombineInstance[] meshCombine = new CombineInstance[meshFilters.Length];
-        for(int i =0;i<meshFilters.Length;i++)
-        {
-            meshCombine[i].mesh = meshFilters[i].sharedMesh;
+        //MeshFilter[] meshFilters = Selection.activeGameObject.GetComponentsInChildren<MeshFilter>();
+        //CombineInstance[] meshCombine = new CombineInstance[meshFilters.Length];
+        //for(int i =0;i<meshFilters.Length;i++)
+        //{
+        //    meshCombine[i].mesh = meshFilters[i].sharedMesh;
 
-            meshCombine[i].transform = meshFilters[i].transform.localToWorldMatrix;
-        }
+        //    meshCombine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+        //}
 
-        Mesh mesh = new Mesh();
-        mesh.CombineMeshes(meshCombine);
-        GameObject obj = new GameObject(Selection.activeGameObject.name);
-        obj.AddComponent<MeshFilter>().mesh = mesh;
-        obj.AddComponent<MeshRenderer>().sharedMaterial = meshFilters[0].gameObject.GetComponent<MeshRenderer>().sharedMaterial;
+        //Mesh mesh = new Mesh();
+        //mesh.CombineMeshes(meshCombine);
+        //GameObject obj = new GameObject(Selection.activeGameObject.name);
+        //obj.AddComponent<MeshFilter>().mesh = mesh;
+        //obj.AddComponent<MeshRenderer>().sharedMaterial = meshFilters[0].gameObject.GetComponent<MeshRenderer>().sharedMaterial;
     }
 
 }
